9f05033dbf1cae447f748044e7ca22fc7848fe35
narco62800 <narco62800@gmail.com>
2026-04-18 09:38:53 +0200

    correct Supabase anon key

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = "https://lwaccthqrjcocyamaczn.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "ta-clé-anon-icisb_secret_2crP1mXj5SuivKOQX2ZmWA_z90gGuLr";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3YWNjdGhxcmpjb2N5YW1hY3puIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0Mzc3MDksImV4cCI6MjA5MjAxMzcwOX0.L70ywsKeHsNcNN9w2e8GQPjF7ylra9iTuIKxIq8aFZI"; // anon key
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
