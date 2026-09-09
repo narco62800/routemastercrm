b54da4d10bee0a8c5b93b80f7cc39a71cf9dd4a2
narco62800 <narco62800@gmail.com>
2026-09-09 21:44:04 +0200

    Update index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,5 @@
 import { Question, Chapter } from '../types';
 import { INITIAL_CHAPTERS } from './chapters';
-{ level: "Terminales CRM", subject: "Cours", title: "Bilan première" },
-  { level: "1ères CRM", subject: "Cours", title: "Bilan première" },
 import { ETG_QUESTIONS } from './questions_etg';
 import { RSE_QUESTIONS } from './questions_rse';
 import { ECRITES_C_QUESTIONS } from './questions_ecrites_c';
