018969a402df36840473d4240a47fa899d50f89c
narco62800 <narco62800@gmail.com>
2026-09-09 20:28:42 +0200

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "https://lwaccthqrjcocyamaczn.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || "sb_secret_2crP1mXj5SuivKOQX2ZmWA_z90gGuLr";
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3YWNjdGhxcmpjb2N5YW1hY3puIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0Mzc3MDksImV4cCI6MjA5MjAxMzcwOX0.L70ywsKeHsNcNN9w2e8GQPjF7ylra9iTuIKxIq8aFZI";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
